532c0604f078520f577d36fcf954c90f5c9503aa
Sniffin3083 <thaxterg12@gmail.com>
2024-06-23 10:29:30 -0600

    Greeting and navigation set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-zinc-800 h-screen">
+       <div>
+        <div className="absolute right-[500px] top-32">
+          <div className="group grid grid-cols-3 hover:font-bold">
+            <div className="bg-zinc-800 group-hover:bg-sky-700 w-10 rounded-full">
+              <div class="flex flex-row justify-center items-center w-10 h-10 border border-1 border-black rounded-full">
+                <div class="w-5 h-5 border border-1 border-black rounded-full" />
+              </div>
+            </div>
+            <hr className="h-px mt-5 w-48 ml-[-80px] bg-black border-0" />
+            <h2 className="text-white text-2xl ml-[-10px] font-glegoo">Introduction</h2>
+          </div>
+          <div className="group grid grid-cols-3 hover:font-bold pt-2">
+            <div className="bg-zinc-800 group-hover:bg-sky-700 w-10 rounded-full">
+              <div class="flex flex-row justify-center items-center w-10 h-10 border border-1 border-black rounded-full">
+                <div class="w-5 h-5 border border-1 border-black rounded-full" />
+              </div>
+            </div>
+            <hr className="h-px mt-5 w-48 ml-[-80px] bg-black border-0" />
+            <h2 className="text-white text-2xl ml-[-10px] font-glegoo">About Me</h2>
+          </div>
+          <div className="group grid grid-cols-3 hover:font-bold pt-2">
+            <div className="bg-zinc-800 group-hover:bg-sky-700 w-10 rounded-full">
+              <div class="flex flex-row justify-center items-center w-10 h-10 border border-1 border-black rounded-full">
+                <div class="w-5 h-5 border border-1 border-black rounded-full" />
+              </div>
+            </div>
+            <hr className="h-px mt-5 w-48 ml-[-80px] bg-black border-0" />
+            <h2 className="text-white text-2xl ml-[-10px] font-glegoo">Education</h2>
+          </div>
+          <div className="group grid grid-cols-3 hover:font-bold pt-2">
+            <div className="bg-zinc-800 group-hover:bg-sky-700 w-10 rounded-full">
+              <div class="flex flex-row justify-center items-center w-10 h-10 border border-1 border-black rounded-full">
+                <div class="w-5 h-5 border border-1 border-black rounded-full" />
+              </div>
+            </div>
+            <hr className="h-px mt-5 w-48 ml-[-80px] bg-black border-0" />
+            <h2 className="text-white text-2xl ml-[-10px] font-glegoo">Tech Stack</h2>
+          </div>
+          <div className="group grid grid-cols-3 hover:font-bold pt-2">
+            <div className="bg-zinc-800 group-hover:bg-sky-700 w-10 rounded-full">
+              <div class="flex flex-row justify-center items-center w-10 h-10 border border-1 border-black rounded-full">
+                <div class="w-5 h-5 border border-1 border-black rounded-full" />
+              </div>
+            </div>
+            <hr className="h-px mt-5 w-48 ml-[-80px] bg-black border-0" />
+            <h2 className="text-white text-2xl ml-[-10px] font-glegoo">Hire Me</h2>
+          </div>
+          <div className="group grid grid-cols-3 hover:font-bold pt-2">
+            <div className="bg-zinc-800 group-hover:bg-sky-700 w-10 rounded-full">
+              <div class="flex flex-row justify-center items-center w-10 h-10 border border-1 border-black rounded-full">
+                <div class="w-5 h-5 border border-1 border-black rounded-full" />
+              </div>
+            </div>
+            <hr className="h-px mt-5 w-48 ml-[-80px] bg-black border-0" />
+            <h2 className="text-white text-2xl ml-[-10px] font-glegoo">Projects</h2>
+          </div>
+          <div className="group grid grid-cols-3 hover:font-bold pt-2">
+            <div className="bg-zinc-800 group-hover:bg-sky-700 w-10 rounded-full">
+              <div class="flex flex-row justify-center items-center w-10 h-10 border border-1 border-black rounded-full">
+                <div class="w-5 h-5 border border-1 border-black rounded-full" />
+              </div>
+            </div>
+            <hr className="h-px mt-5 w-48 ml-[-80px] bg-black border-0" />
+            <h2 className="text-white text-2xl ml-[-10px] font-glegoo">References</h2>
+          </div>
+          <div className="group grid grid-cols-3 hover:font-bold pt-2">
+            <div className="bg-zinc-800 group-hover:bg-sky-700 w-10 rounded-full">
+              <div class="flex flex-row justify-center items-center w-10 h-10 border border-1 border-black rounded-full">
+                <div class="w-5 h-5 border border-1 border-black rounded-full" />
+              </div>
+            </div>
+            <hr className="h-px mt-5 w-48 ml-[-80px] bg-black border-0" />
+            <h2 className="text-white text-2xl ml-[-10px] font-glegoo">Conclusion</h2>
+          </div> 
+        </div>
+      </div>
+      <div className="pl-16 pt-12">
+        <h1 className="text-orange-500 font-bold text-5xl font-inter">Hi NDP Party of BC</h1>
+        <p className="mt-8 pr-24 w-screen lg:w-1/2 text-white font-medium text-xl font-inter">I wanted to stand out with my resume so I decided to turn it into a website for you to navigate around. I have also included a download link to a more proper resume at the bottom of this page. I hope you enjoy learning more about me and my skill set. If you have any questions at all feel free to reach out at any time.</p>
+      </div>  
     </div>
   );
 }
